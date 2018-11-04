fb1c64bd0b5463e44543b13972759bb1493346de
undersourcecode <undersourcecode@hotmail.co.jp>
2018-11-04 18:34:50 +0900

    withdrawを実装。

--- chapter5/sampleDdd/src/main/java/com/example/sampleDdd/repository/BankAccountDatasource.java
+++ chapter5/sampleDdd/src/main/java/com/example/sampleDdd/repository/BankAccountDatasource.java
@@ -38,7 +38,28 @@ public class BankAccountDatasource implements BankAccountRepository{
 
     @Override
     public void withdraw(Amount amount) {
-		// TODO: implement
-        System.out.println("withdraw");
+		try{
+            Class.forName("org.sqlite.JDBC");
+
+            try(Connection connection = DriverManager.getConnection("jdbc:sqlite:sample.db")){
+                Statement statement = connection.createStatement();
+                statement.setQueryTimeout(30);
+
+                ResultSet rs = statement.executeQuery("select * from amount");
+                rs.next();
+                final int currentValue = rs.getInt("value");
+                final int updateValue = currentValue - amount.value;
+                statement.executeUpdate("update amount set value =  " + updateValue);
+
+                ResultSet updated = statement.executeQuery("select * from amount");
+                updated.next();
+                final int updatedValue = rs.getInt("value");
+                System.out.println("amount updated " + updatedValue);
+            }catch(SQLException e){
+                throw new RuntimeException(e);
+            }
+        }catch(ClassNotFoundException e){
+            throw new RuntimeException(e);
+        }
 	}
 }
